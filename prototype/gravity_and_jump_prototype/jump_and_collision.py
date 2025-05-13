d2e6a8d11366d3244d7daec355018cf33b810ad2
PC <denzaz19@highgateschool.org.uk>
2025-05-13 23:28:14 +0100

    somewhat made jump work, need to work on collisions now

--- prototype/gravity_and_jump_prototype/jump_and_collision.py
+++ prototype/gravity_and_jump_prototype/jump_and_collision.py
@@ -1,7 +1,10 @@
+#can't debug the jump function properly for some reason
+
+
 import math
 import random
 import pygame
-
+from pygame import K_SPACE
 
 SCREEN_WIDTH = 0
 SCREEN_HEIGHT = 0
@@ -21,6 +24,7 @@ class Game:
         PLAYER_WIDTH = pygame.display.get_window_size()[0] // 200
         PLAYER_HEIGHT = pygame.display.get_window_size()[1] // 200
         self.image = pygame.Surface([PLAYER_WIDTH, PLAYER_HEIGHT])
+        self.player=Player()
 
 
     def pygame_init(self):
@@ -29,6 +33,7 @@ class Game:
 
     def main_screen(self):
         # game loop
+        self.create_player()
         while self.running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -41,10 +46,9 @@ class Game:
 
             self.draw_background()
 
+            self.player.jump(keys)
 
-
-
-            #self.update_gravity()
+            self.update_gravity()
 
             self.endframe()
 
@@ -57,12 +61,12 @@ class Game:
 
     def draw_background(self):
         self.screen.fill((135, 200, 235))
+        self.screen.blit(self.player.surf, self.player.rect)
 
     def update_gravity(self):
-        for sprite in self.sprites:
-            if sprite.get_on_ground() and sprite.gravity < 5:
-                sprite.gravity += 1
-            sprite.rect.move_ip(0, sprite.gravity)
+        if self.player.get_on_ground() and self.player.gravity < 11:
+            self.player.gravity += 1
+        self.player.rect.move_ip(0, self.player.gravity)
 
     def endframe(self):
         # updating display and game
@@ -77,11 +81,20 @@ class Player(pygame.sprite.Sprite):
 
         self.surf = pygame.Surface((75, 25))
         self.surf.fill((255, 255, 255))
-        self.rect = self.surf.get_rect()
-        self.on_ground = False
+        self.rect = pygame.Rect(500, 500, 64, 64)
+        self.on_ground = True
+        self.gravity=0
+        self.velocity=1
 
+    #need to delete the last (self.on_ground=True) from here and add it to the collisions function when i make it
     def jump(self, key):
-        pass
+        if key[pygame.K_SPACE]:
+            self.gravity=0
+            self.on_ground = False
+            for i in range(10,0,-1):
+                self.rect.move_ip(0, -i)
+                game.draw_background()
+        self.on_ground = True
 
     def collisions(self):
         pass
