0675fdd7d57cb1aa2b59707bf84b3168853cdd7b
denzaz19 <denzaz19@highgateschool.org.uk>
2025-07-30 00:14:58 +0100

    used https://www.youtube.com/watch?v=ML2w92TIzoA&list=PLjcN1EyupaQnHM1I9SmiXfbT6aG4ezUvu&index=6 to understand pygame.sprite.Sprite and then added enemies (without collision with the player)

--- main_/game.py
+++ main_/game.py
@@ -6,7 +6,7 @@ SCREEN_HEIGHT = 960 #900
 TILE_SIZE=64 #50
 FPS = 60
 
-class Game:
+class Game():
     def __init__(self):
         # general setup
         self.setting_up()
@@ -16,6 +16,7 @@ class Game:
         self.running = True
         self.count = 1
         self.gravity = 0.75
+        self.level_count = 1
 
     def setting_up(self):
         pygame.init()
@@ -30,6 +31,7 @@ class Game:
         # game loop
         self.create_player(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
         while self.running:
+            #if self.level_count==1:
             self.clock.tick(FPS)
 
             keys=self.input_handling()
@@ -59,13 +61,14 @@ class Game:
 
     def update_movement(self,keys):
         self.vandi.move(keys, self.world)
-
+        self.world.sharks.update(keys)
 
     def draw(self):
         self.screen.fill((50, 50, 50))
         self.draw_grid()
 
-        self.level1()
+        self.level1_load()
+        self.world.sharks.draw(self.screen)
 
         self.screen.blit(self.vandi.img, self.vandi.rect)
 
@@ -75,7 +78,7 @@ class Game:
             pygame.draw.line(self.screen, (255, 255, 255), (line * TILE_SIZE, 0), (line * TILE_SIZE, SCREEN_HEIGHT))
 
 
-    def level1(self):
+    def level1_load(self):
         layout=[
             ['B20'],
             ['B1', 'A18', 'B1'],
@@ -87,17 +90,20 @@ class Game:
             ['B1', 'A18', 'B1'],
             ['B1', 'A18', 'B1'],
             ['B1', 'A18', 'B1'],
-            ['B1', 'A18', 'B1'],
+            ['B1', 'A17', 'S1', 'B1'],
             ['A16', 'B4'],
             ['A15', 'B5'],
-            ['A14', 'B6'],
+            ['A8', 'S1', 'A5', 'B6'],
             ['A3', 'B17'],
         ]
-        self.world=World(layout)
-        level1=self.world.load_level()
+        if self.level_count==1:
+            self.world=World(layout)
+            self.level1=self.world.load_level()
 
-        for tile in level1:
+        for tile in self.level1:
             self.screen.blit(tile[0], tile[1])
+
+        self.level_count+=1
 
     def endframe(self):
         # updating display and game
@@ -111,6 +117,7 @@ class World():
         self.data = data
 
         self.block=pygame.image.load(f'assets/blocks/block.jpg')
+        self.sharks=pygame.sprite.Group()
 
         self.tile_count=0
         self.row_count=0
@@ -122,6 +129,7 @@ class World():
             for tile in row:
                 if tile[0]=='A':
                     self.tile_count+=int(tile[1:])
+
                 if tile[0]=='B':
                     for i in range(int(tile[1:])):
                         img=pygame.transform.scale(self.block,(TILE_SIZE,TILE_SIZE))
@@ -134,11 +142,18 @@ class World():
 
                         self.tile_count = self.tile_count + 1
 
+                if tile[0]=='S':
+                    # (TILE_SIZE * self.row_count - ((TILE_SIZE * (self.row_count)) - (TILE_SIZE * (self.row_count + 1))))
+                    shark=Enemy(TILE_SIZE * self.tile_count, (TILE_SIZE * self.row_count), 'shark', 2)
+                    self.sharks.add(shark)
+
+                    self.tile_count += 1
+
             self.row_count+=1
 
         return self.tile_list
 
-class Player(pygame.sprite.Sprite):
+class Player():
     def __init__(self, width, height, x, y):
         self.images_right=[]
         self.images_left = []
@@ -235,6 +250,53 @@ class Player(pygame.sprite.Sprite):
             if tile[1].colliderect(self.rect.x, self.rect.y + self.velocity[1] + game.gravity, self.width, self.height):
                 self.on_ground = True
 
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self, x, y, name, size_scale):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.images_right = []
+        self.images_left = []
+        self.index = 0
+        self.counter = 0
+        for i in range(1, 3):
+            img = pygame.image.load(f'assets/enemy/idle/{name}{i}.png').convert_alpha()
+            img = pygame.transform.scale(img,  (img.get_width()*size_scale, img.get_height()*size_scale))
+            self.images_right.append(img)
+            img_left = pygame.transform.flip(img, True, False)
+            self.images_left.append(img_left)
+
+        self.image = self.images_right[self.index]
+        self.rect=self.image.get_rect(center=(x,y))
+        self.rect.y += y + 64 - self.rect.bottom
+        self.direction = 1
+        self.distance_tracker = 0
+
+    def update(self, keys):
+        self.rect.x += self.direction
+        self.distance_tracker += 1
+
+        if abs(self.distance_tracker) >= 64:
+            self.direction *= -1
+            self.distance_tracker *= -1
+
+        self.counter += 1
+        if self.counter > 5:
+            self.counter = 0
+
+            if self.direction>0:
+                self.index += 1
+                if self.index >= len(self.images_right):
+                    self.index = 0
+
+            elif self.direction<0:
+                self.index += 1
+                if self.index >= len(self.images_left):
+                    self.index = 0
+
+            if self.direction > 0:
+                self.image = self.images_right[self.index]
+            elif self.direction < 0:
+                self.image = self.images_left[self.index]
 
 
 if __name__ == '__main__':
